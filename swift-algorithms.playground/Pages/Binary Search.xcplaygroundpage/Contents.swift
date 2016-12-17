b280d63fc350edc981825b36a2cffa9a7a39c963
mattthousand <matt.buckley@raizlabs.com>
2016-12-17 15:51:10 -0500

    Commenting

--- swift-algorithms.playground/Pages/Binary Search.xcplaygroundpage/Contents.swift
+++ swift-algorithms.playground/Pages/Binary Search.xcplaygroundpage/Contents.swift
@@ -1,7 +1,64 @@
-//: [Previous](@previous)
+/**
+ Binary Search
+ 
+ Recursively splits the array in half until the value is found.
+ 
+ If there is more than one occurrence of the search key in the array, then
+ there is no guarantee which one it finds.
+ 
+ Note: The array must be sorted!
+ **/
 
 import Foundation
 
-var str = "Hello, playground"
+public typealias Index = Int
 
-//: [Next](@next)
+enum SearchError: Error { case unsortedInput, searchKeyNotPresent }
+
+extension Array where Element: Comparable {
+    var isSorted: Bool {
+        for idx in 1..<count {
+            if self[idx - 1] > self[idx] { return false }
+        }
+        return true
+    }
+}
+
+// The recursive version of binary search.
+
+public func myBinarySearch<T: Comparable>(_ sortedCollection: [T], searchKey: T, idxRange: Range<Index>) throws -> Index? {
+
+    guard sortedCollection.isSorted else { throw SearchError.unsortedInput }
+    guard idxRange.upperBound > idxRange.lowerBound else { throw SearchError.searchKeyNotPresent }
+
+    let midIdx = idxRange.lowerBound + (idxRange.upperBound - idxRange.lowerBound) / 2
+
+    switch midIdx {
+    case let i where sortedCollection[i] > searchKey:
+        return try myBinarySearch(sortedCollection, searchKey: searchKey, idxRange: idxRange.lowerBound..<midIdx)
+    case let i where sortedCollection[i] < searchKey:
+        return try myBinarySearch(sortedCollection, searchKey: searchKey, idxRange: (midIdx + 1)..<idxRange.upperBound)
+    default:
+        return midIdx
+    }
+    
+}
+
+// An unsorted array of numbers
+let numbers = [11, 59, 3, 2, 53, 17, 31, 7, 19, 67, 47, 13, 37, 61, 29, 43, 5, 41, 23]
+
+// Binary search requires that the array is sorted from low to high
+let sorted = numbers.sorted()
+
+do {
+    // Using recursive solution
+    try myBinarySearch(sorted, searchKey: 2, idxRange: 0 ..< sorted.count)   // gives 0
+    try myBinarySearch(sorted, searchKey: 67, idxRange: 0 ..< sorted.count)  // gives 18
+    try myBinarySearch(sorted, searchKey: 43, idxRange: 0 ..< sorted.count)  // gives 13
+    try myBinarySearch(sorted, searchKey: 42, idxRange: 0 ..< sorted.count)  // throws searchKeyNotPresent error
+
+}
+catch {
+    print("error \(error)")
+}
+
--- swift-algorithms.playground/Pages/Insertion Sort.xcplaygroundpage/Contents.swift
+++ swift-algorithms.playground/Pages/Insertion Sort.xcplaygroundpage/Contents.swift
@@ -1,12 +1,31 @@
+/**
+ Insertion Sort
+
+ Insertion sort iterates, consuming one input element each repetition,
+ and growing a sorted output list. Each iteration, insertion sort removes 
+ one element from the input data,finds the location it belongs within the 
+ sorted list, and inserts it there. It repeats until no input elements remain.
+ 
+ Performance:
+ - O(n^2) <- worst case
+ 
+ **/
+
 func insertionSort<T: Comparable>(_ collection: [T]) -> [T] {
 
+    // The collection is already sorted if it's empty, so just return it
     guard collection.count > 0 else {
         return collection
     }
 
+    // We need to copy the collection passed in, because it's immutable
     var collectionCopy = collection
 
+    // Iterate through the collection
     for idx in 1..<collection.count {
+
+        // compare 2 adjacent elements, if they are out of order, swap them 
+        // and pass the altered collection back into the insertionSort
         if collection[idx] < collection[idx - 1] {
             swap(&collectionCopy[idx], &collectionCopy[idx - 1])
             return insertionSort(collectionCopy)
